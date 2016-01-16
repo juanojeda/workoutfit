5c3db205399517f70ccaa7e41ba8fbc4ef2c86b0
Juan Ojeda <juan@juanojeda.com>
2016-01-17 00:51:59 +1100

    set up view for create workout, listing exercises based on exercise database, disabling save based on workout exercises or tempexercise form

--- client/workouts/create/create-workout.component.js
+++ client/workouts/create/create-workout.component.js
@@ -4,8 +4,38 @@ Workoutfit
             restrict: 'E',
             templateUrl: 'client/workouts/create/create-workout.html',
             controllerAs: 'createWorkout',
-            controller: function($scope, $stateParams, $reactive){
+            controller: function($scope, $stateParams, $reactive, $state){
                 $reactive(this).attach($scope);
+
+                this.cancelCreate = function(){
+                    $state.transitionTo('workouts');
+                };
+
+                this.canSubmit = function(){
+                    if (_.isEmpty(this.workout.exercises)){
+                        return !!this.tempExercise.exerciseId;
+                    } else {
+                        return true;
+                    };
+                };
+
+                this.helpers({
+                    tempExercise: () => {
+                        return {}
+                    },
+                    workout: () => {
+                        return {
+                            name: '',
+                            exercises: []
+                        }
+                    },
+                    saveWorkout: () => {
+                        // save the workout to the database
+                    },
+                    exerciseList: () => {
+                        return Exercises.find({});
+                    }
+                });
             }
         }
     });
