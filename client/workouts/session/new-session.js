c363ee6e718106cdd454625435f5ab00635aa3df
Juan Ojeda <juan@juanojeda.com>
2016-02-25 23:47:57 +1100

    added workout progress meter, added workout iterator function (to progress to next exercise and increase prog meter), added complete status indication

--- client/workouts/session/new-session.js
+++ client/workouts/session/new-session.js
@@ -10,8 +10,34 @@ Workoutfit
                 var newSession = this;
 
                 this.helpers({
-                    workout: GetWorkoutFactory($stateParams.workoutId)
+                    workout: GetWorkoutFactory($stateParams.workoutId),
+                    workoutCurrentState: 0,
+                    totalWorkoutLength: 0,
+                    workoutProgress: function(){
+                        var progress = 0;
+                        if (newSession.workoutCurrentState > 0){
+                            progress = (newSession.workoutCurrentState / newSession.totalWorkoutLength) * 100;
+                        }
+                        return progress;
+                    },
+                    workoutComplete: function(){
+                        return newSession.workoutProgress >= 100;
+                    }
                 });
+
+                newSession.incrementWorkout = function(){
+                    newSession.workoutCurrentState++;
+                };
+
+                // set newSession.totalWorkoutLength
+                _.each(newSession.workout.exercises, function(superset){
+                    _.each(superset, function(exercise){
+                        newSession.totalWorkoutLength += exercise.sets;
+                    });
+                })
+
+                // TODO: remove this
+                console.log("%c %s %s", "color: #c0ffee; background: #323266", "Hey Juan! ", "newSession.workout, newSession.totalWorkoutLength!", newSession.workout, newSession.totalWorkoutLength);
             }
         }
     });
